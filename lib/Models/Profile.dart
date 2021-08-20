bffb9eb8e6b8d7933ae743e3ca54ff87bbb4e8bd
Aman Raj <amanpie9@gmail.com>
2021-08-20 19:07:25 +0530

    ui desings done

---
+++ lib/Models/Profile.dart
@@ -0,0 +1,7 @@
+class Person {
+  String name, imgurl, about, email, id;
+  List<Person> friends;
+  List<Person> blocked;
+  
+  Person(this.name, this.about, this.email, this.id, this.imgurl, this.friends,this.blocked);
+}
--- lib/screens/lobby.dart
+++ lib/screens/lobby.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class WaitingLobby extends StatelessWidget {
   const WaitingLobby({Key? key}) : super(key: key);
@@ -7,6 +8,7 @@ class WaitingLobby extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        titleSpacing: 15.w,
         title: Text("7594fh3"),
         leading: Icon(Icons.backspace_rounded),
         actions: [
@@ -17,18 +19,93 @@ class WaitingLobby extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.person),
-                Text("Dr Krishna Kumar"),
-                Row(
-                  children: [
-                    Icon(Icons.person_add),
-                    Icon(Icons.block),
-                  ],
-                )
-              ],
+            Container(
+              margin: EdgeInsets.only(top: 0.3.h),
+              color: Colors.lightBlueAccent,
+              height: 10.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(left: 8.w),
+                      child: Icon(Icons.person)),
+                  Text("Dr Krishna Kumar"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.person_add),
+                      SizedBox(width: 10),
+                      Icon(Icons.block),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 0.3.h),
+              color: Colors.lightBlueAccent,
+              height: 10.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(left: 8.w),
+                      child: Icon(Icons.person)),
+                  Text("Dr Krishna Kumar"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.person_add),
+                      SizedBox(width: 10),
+                      Icon(Icons.block),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 0.3.h),
+              color: Colors.lightBlueAccent,
+              height: 10.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(left: 8.w),
+                      child: Icon(Icons.person)),
+                  Text("Dr Krishna Kumar"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.person_add),
+                      SizedBox(width: 10),
+                      Icon(Icons.block),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 0.3.h),
+              color: Colors.lightBlueAccent,
+              height: 10.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(left: 8.w),
+                      child: Icon(Icons.person)),
+                  Text("Dr Krishna Kumar"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.person_add),
+                      SizedBox(width: 10),
+                      Icon(Icons.block),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
